42a8f364ef552ab9ab781bb29a81a0423d204c59
Rosie <Rosie@1x-vl943-72-19-94-222.wireless.umass.edu>
2015-10-24 17:05:15 -0400

    Adding core data framework

--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -19,19 +19,8 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let date1 = Date.from(2014, month: 05, day: 20)
-        let date2 = Date.from(2014, month: 03, day: 3)
-        let date3 = Date.from(2014, month: 12, day: 13)
         
-        let task1 = taskModel(task: "Study French", subtask: "Verbs", date: date1, completed: false)
-        let task2 = taskModel(task: "Eat Dinner", subtask: "Burgers", date: date2, completed: false)
-        let taskArray = [task1, task2, taskModel(task: "Gym", subtask: "Leg Day", date: date3, completed: false)]
-
-        var completedArray = [taskModel(task: "code", subtask: " ", date: date2, completed: true)]
-
-        baseArray = [taskArray, completedArray]
-
-        self.tableView.reloadData()
+        
     }
     
     // Refresh information insdie of the table view
